f05e7078adf42e7cdb78fa32b93f0c827333738c
steveu <steve@urm.st>
2021-03-12 11:47:39 +0000

    Fix basic included text

--- src/components/application/application.test.js
+++ src/components/application/application.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Application from './application';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders title', () => {
+  render(<Application />);
+  const titleElement = screen.getByText(/Login Challenge/i);
+  expect(titleElement).toBeInTheDocument();
 });
