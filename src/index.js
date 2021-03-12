2bd1b681e5f69001b95535aa04afcdbc8375cfb2
steveu <steve@urm.st>
2021-03-12 11:41:07 +0000

    Add SCSS support and start adjusting base structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'reset-css';
 import './index.css';
-import App from './App';
+import Application from './components/application/application';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>,
   document.getElementById('root')
 );
